8c3b9c6f8873906d8e9200c6a6e85953bfe327a8
rasmith78 <roselynsmith78@gmail.com>
2016-10-12 13:50:37 -0600

    adjust php require once to index.php

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-<<<<<<< HEAD
     <?php require_once("lib/headUtils.php");?>
-=======
-    <?php require_once("linb?headUtils.php"); ?>
->>>>>>> refs/remotes/origin/master
   </head>
   <body>
 
