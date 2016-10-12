ebec276d77c46d145e8eb21d042c5c2089fb06ae
rasmith78 <roselynsmith78@gmail.com>
2016-10-12 15:21:35 -0600

    added jumbo tro added index css etc

--- index.php
+++ index.php
@@ -4,10 +4,91 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headUtils.php"); ?>
+    <link rel="stylesheet" href="lib/css/index.css">
   </head>
-  <body>
 
+  <body>
     <?php require_once("lib/partials/navBar.php"); ?>
+      <div class="container">
+      <div class="jumbotron">
+        <h1>This is my index page.</h1>
+        <p>Welcome. You are more than welcome to browse around</p>
+        <div class="row">
+        <div classs="col-md-3 col-md-offset-9">
+          <button class="btn-lg">Learn More!</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Headline</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+             culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="col-md-6 col-md-offsett-6">
+          <button class="btn-sm btn-danger">Click to learn more.</button>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>Headline</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+               ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                 culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div class="col-md-6 col-md-offsett-6">
+              <button class="btn-sm btn-danger">Click to learn more.</button>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <h2>Headline</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                   ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                     culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div class="col-md-6 col-md-offsett-6">
+              <button class="btn-sm btn-danger">Click to learn more.</button>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <h2>Headline</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                   ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                     culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div class="col-md-6 col-md-offsett-6">
+              <button class="btn-sm btn-danger">Click to learn more.</button>
+            </div>
+          </div>
 
   </body>
+
 </html>
