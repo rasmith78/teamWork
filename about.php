3a77d4eb8a0c462473964556e63192644963ebcf
Bedonie <phelanzobedoniecc@gmail.com>
2016-10-12 15:23:53 -0600

    jumbotron,lorem etc

--- about.php
+++ about.php
@@ -4,9 +4,41 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headUtils.php"); ?>
+    <link rel="stylesheet" href="lib/css/about.css")
   </head>
   <body>
 
     <?php require_once("lib/partials/navBar.php"); ?>
+
+<div class="container">
+  <div class="jumbotron">
+      <h1>Hello, this is my About page.</h1>
+      <p>Welcome. You are welcome to browse this page.</p>
+      <div class="row">
+        <div class="col-md-3 col-md-offset-9">
+          <button class="btm-lg">Learn more button"></button>
+
+        </div>
+      </div>
+    <div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <h2>Headline</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="col-md-offset-6">
+          <bottom class="btm-large btm-danger">click to learn more.</button>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
   </body>
 </html>
