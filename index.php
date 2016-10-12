f0ab323465e302040c616ea3d10d4a29a67e855f
rasmith78 <roselynsmith78@gmail.com>
2016-10-12 14:14:28 -0600

    added require once to navBar to index.php

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headUtils.php"); ?>
-
   </head>
   <body>
+
+    <?php require_once("lib/partials/navBar.php"); ?>
 
   </body>
 </html>
